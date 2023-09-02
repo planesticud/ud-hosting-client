15e27c8083b8de8b00ce7e153a4d2d216819374b
david millan <damillano93@gmail.com>
2023-09-02 12:49:09 -0500

    add create vulhub cloud

--- src/services/vulhub.js
+++ src/services/vulhub.js
@@ -1,27 +1,28 @@
 import axios from "axios";
-const repositoryUrl = 'http://localhost:3000'
+const vulhubUrl = 'https://desarrollo.planestic.udistrital.edu.co/vulhubapi'
+//'http://localhost:3000'
 
 class VulhubService {
 
     getVulhub() {
         const data = {}
-        return axios.get(`${repositoryUrl}/envs`, { params: data })
+        return axios.get(`${vulhubUrl}/envs`, { params: data })
     }
     deleteVulhubById(id) {
-        return axios.delete(`${repositoryUrl}/envs`, { params: { id } })
+        return axios.delete(`${vulhubUrl}/envs`, { params: { id } })
     }
 
     getVulhubByid(id) {
-        return axios.get(`${repositoryUrl}/envs`, { params: { id } })
+        return axios.get(`${vulhubUrl}/envs`, { params: { id } })
     }
     getVulhubByEmail(email) {
-        return axios.get(`${repositoryUrl}/envs`, { params: { email: email } })
+        return axios.get(`${vulhubUrl}/envs`, { params: { email: email } })
     }
     createVulhub(data) {
-        return axios.post(`${repositoryUrl}/envs`, data)
+        return axios.post(`${vulhubUrl}/envs`, data)
     }
     updateVulhub(data, id) {
-        return axios.put(`${repositoryUrl}/envs`, data, { params: { id } })
+        return axios.put(`${vulhubUrl}/envs`, data, { params: { id } })
     }
 }
    
